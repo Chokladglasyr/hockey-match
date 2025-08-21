8b59576714a0d8b2a3aa946c7ed0ca8a76831bf2
Chokladglasyr <lim_ida@live.com>
2025-08-21 11:21:22 +0200

    feat: added func for signing up

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Form({ eng }: { eng: boolean }) {
+    const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const API_URL = import.meta.env.VITE_API_BASE_URL_LOCAL;
+
+  const addAttendee = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`${API_URL}/admin`, {
+        headers: { "Content-type": "application/json" },
+        method: "POST",
+        body: JSON.stringify({ ...formData }),
+      });
+      const data = await res.json();
+      console.log(data);
+      alert("Thank you for signing up!")
+      navigate("/")
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.error("Failed to sign up: ", err);
+      }
+    }
+  };
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value, type } = e.target;
+    if(type === "checkbox" && e.target instanceof HTMLInputElement){
+        setFormData({...formData, [name]: e.target.checked})
+    }else {
+
+        setFormData({ ...formData, [name]: value });
+    }
+  };
   const text = {
     en: {
       name: "Name",
@@ -7,7 +42,8 @@ function Form({ eng }: { eng: boolean }) {
       allergy: "Do you have any allergies/intolerances?",
       other: "Other",
       btn: "Skicka in",
-      thanks: "Tack för att du anmäler dig, då kan vi lättare räkna på hur mycket mat som behövs och hjälpa hotellet få plats med alla!"
+      thanks:
+        "Tack för att du anmäler dig, då kan vi lättare räkna på hur mycket mat som behövs och hjälpa hotellet få plats med alla!",
     },
     sv: {
       name: "Namn",
@@ -16,7 +52,8 @@ function Form({ eng }: { eng: boolean }) {
       allergy: "Har du några allergier/intoleranser?",
       other: "Övrigt",
       btn: "Send",
-      thanks: "Thank you for registering, it makes it easier for us to calculate how much food is needed and help the hotel accommodate everyone!"
+      thanks:
+        "Thank you for registering, it makes it easier for us to calculate how much food is needed and help the hotel accommodate everyone!",
     },
   };
   const lang = eng ? text.en : text.sv;
@@ -25,11 +62,25 @@ function Form({ eng }: { eng: boolean }) {
       <section className="signup">
         <h1>Tack</h1>
         <p>{lang.thanks}</p>
-        <form action="">
-          <input type="text" name="name" id="name" placeholder="Name" required/>
-          <input type="text" name="email" id="email" placeholder="Email" required/>
+        <form onSubmit={addAttendee}>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            required
+            onChange={handleInput}
+          />
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+            onChange={handleInput}
+          />
           <div>
-            <input type="checkbox" name="hotel" id="hotel" />
+            <input type="checkbox" name="staying" id="staying" onChange={handleInput}/>
             <label htmlFor="hotel">{lang.hotel}</label>
           </div>
           <div>
@@ -37,19 +88,21 @@ function Form({ eng }: { eng: boolean }) {
               type="checkbox"
               name="banquet"
               id="banquet"
-              value={"banquet"}
+              onChange={handleInput}
             />
             <label htmlFor="banquet">{lang.banquet}</label>
           </div>
           <div className="text">
             <label htmlFor="allergy">{lang.allergy}</label>
-            <textarea name="allergy" id="allergy"></textarea>
+            <textarea name="allergy" id="allergy" onChange={handleInput}></textarea>
           </div>
           <div className="text">
             <label htmlFor="other">{lang.other}</label>
-            <textarea name="other" id="other" ></textarea>
+            <textarea name="other" id="other" onChange={handleInput}></textarea>
           </div>
-          <button className="BTN" id="signup">{lang.btn}</button>
+          <button className="BTN" id="signup">
+            {lang.btn}
+          </button>
         </form>
       </section>
     </>
